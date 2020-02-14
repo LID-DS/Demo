c95cd57e1682f621a66d1312060e48eb53ff4280
tk <tim_k@posteo.de>
2020-02-14 14:17:51 +0100

    Added React file

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.css';
-import { w3cwebsocket as W3CWebSocket } from "websocket";
-
-const URL = 'ws://127.0.0.1:5000'
+import io from 'socket.io-client'
 
 class App extends React.PureComponent{
+
     constructor(props){
 	super(props)
 	this.state = {
 	    sum : 0
 	}
-	this.client = new W3CWebSocket(URL)
+	console.log('start websocket')
     }
 
     render(){
@@ -19,7 +18,28 @@ class App extends React.PureComponent{
 	)
     }
     componentDidMount(){
-	this.client.onopen = () => this.client.send("read")
+	
+	var socket = io('ws://localhost:5000/');
+	socket.on('connect', function() {
+	    console.log('connected')
+	    socket.emit('get stats', 'sum')
+	    //socket.emit('my event', {
+		//data : 'Connected'
+	    //});
+	});
+	socket.on('my response', function(data) {
+	    console.log("somethin");
+	});
+	socket.on('stats_sum', function(data) {
+	    console.log(data["sum"])
+	    this.setState({
+		sum : data["sum"]
+	    })
+	}.bind(this));
+
+    }
+    componentWillUnmount() {
+	clearInterval(this.interval);
     }
 }
 
