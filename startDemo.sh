cb83f1ec8545713bf535c8efe8d7e581459d4c1d
Martin Grimmer <mgrimmer42@gmail.com>
2020-03-03 18:49:35 +0100

    changed vulnerable app to owasp juice shop

--- startDemo.sh
+++ startDemo.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #start docker
-sudo docker run -d 'vulhub/mysql:5.5.23'
+#sudo docker run -d 'vulhub/mysql:5.5.23'
+sudo docker run --rm -p 3000:3000 bkimminich/juice-shop
 
 #start webserver
 python3 webserver.py &
