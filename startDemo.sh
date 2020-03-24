f42a10f8e33afd819d6ee0b83f65aee22207455c
demo <you@example.com>
2020-03-24 16:41:46 +0100

    corrected commands

--- startDemo.sh
+++ startDemo.sh
@@ -2,7 +2,6 @@
 
 #install prequesites for downloading and installing needed features
 sudo apt update
-sudo apt install git -y
 sudo apt install curl -y
 sudo apt install python3-pip -y
 
@@ -10,14 +9,23 @@ sudo apt install python3-pip -y
 #Frontend
 #React
 # install nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.2/install.s | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.2/install.sh | bash
+#TODO restart terminal so command nvm is usable
+#try
+source ~/.bashrc
 # install node and matching npm via nvm
-nvm install v8.10.0
+nvm install 12.6
+#install demo-app dependencies
+npm install --prefix demo-app/
 #Webshop
-sudo apt install docker -y
+sudo apt install docker.io -y
+#allow docker to run as sudo without entering password
+echo 'demo ALL=(ALL) NOPASSWD: /usr/bin/docker' | sudo tee -a /etc/sudoers
+#sudo apt install nvm
 
 #Backend
 #install python dependencies
+cd ~/Documents/Demo/
 pip3 install -r requirements.txt
 #install sysdig
 curl -s https://s3.amazonaws.com/download.draios.com/stable/install-sysdig | sudo bash
@@ -25,10 +33,14 @@ curl -s https://s3.amazonaws.com/download.draios.com/stable/install-sysdig | sud
 #UserActions
 mkdir Downloads && cd Downloads
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
-sudo apt install ./Downloads/google-chrome-stable_current_amd64.deb -y
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
+cd ..
 sudo apt install chromium-chromedriver -y
 
 sudo apt update
+
+sudo apt install tmux -y
+sudo apt install tmuxp -y
 
 # tmux session with prepared config which starts:
 #   docker juice shop
@@ -36,4 +48,5 @@ sudo apt update
 #   webserver.py for syscall analysis
 #   userAction.py for automated traffic on juice shop
 
+#TODO userAction wait until page is reachable
 tmuxp load startDemo.yaml
